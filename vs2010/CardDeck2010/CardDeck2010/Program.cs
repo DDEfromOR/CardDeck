c551d1e8ff6b9ab05838af2fddc45bc3114a87aa
Daniel Evans <DDEvans@monsooncommerce.com>
2015-12-15 10:37:54 -0800

    Third time lucky!

--- vs2010/CardDeck2010/CardDeck2010/Program.cs
+++ vs2010/CardDeck2010/CardDeck2010/Program.cs
@@ -1,9 +1,15 @@
-﻿namespace CardDeck2010
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CardDeck2010
 {
    class Program
    {
       static void Main(string[] args)
       {
+         
       }
    }
 }
