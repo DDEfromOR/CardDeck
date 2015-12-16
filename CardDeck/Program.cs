f9f51ceae381962a72fc536292984cb5f172bd80
DDEfromOR <daniel@ddevans.com>
2015-12-16 15:16:09 -0800

    Update Program.cs

--- CardDeck/Program.cs
+++ CardDeck/Program.cs
@@ -7,6 +7,9 @@ namespace CardDeck2010
 {
    class Program
    {
+      /*
+         *Exists to provide an entry point during debugging.
+      */
       static void Main(string[] args)
       {
          
